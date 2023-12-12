c8a28a86c29c24a8ec226795f14c24beeae44055
malwarelabmurphy <malwarelabmurphy@gmail.com>
2023-12-11 20:13:51 -0500

    Adding fuzzer script.

---
+++ Python/murphy_simple_fuzzer.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+# MURPHY'S SIMPLE FUZZER
+
+# Inspiration and walkthrough courtesy of TCM Security: Practical Ethical Hacking <https://academy.tcm-sec.com/p/practical-ethical-hacking-the-complete-course>
+
+import sys
+import socket
+from time import sleep
+
+# Create buffer to test
+
+buffer= "A" * 100
+
+# Set variables for target
+
+ip=input("What is the target IP address?")
+port=input("What is the target port?")
+command=input("What command will we fuzz?")
+
+# Loop for testing buffer
+
+while True:
+    try:
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect((ip,port))
+
+        s.send((command + buffer))
+        s.close()
+        sleep(1)
+        buffer=buffer + "A"*100
+        
+    except:
+        print "Fuzzing attempt crashed at %s bytes!" % str(len(buffer))
+        sys.exit()
